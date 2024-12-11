98e47d2b92fe6e11ccda480ef8bad0d5da947fa3
Aijaz Ali Inayat <webextolcollege@gmail.com>
2024-12-12 00:22:56 +0500

    add remove cart item functionality

--- store/admin.py
+++ store/admin.py
@@ -9,7 +9,7 @@ class ProductAdmin(admin.ModelAdmin):
 class VariationAdmin(admin.ModelAdmin):
     list_display = ('product', 'variation_category', 'variation_value', 'is_active', 'modified_date')
     list_editable = ('is_active',)
-    list_filter = ('product', 'variation_category', 'variation_value', 'is_active')
+    list_filter = ('product', 'variation_category', 'variation_value')
 admin.site.register(Product, ProductAdmin)
 
 admin.site.register(Variation, VariationAdmin)
--- store/models.py
+++ store/models.py
@@ -40,7 +40,7 @@ class Variation(models.Model):
         variation_category = models.CharField(max_length=100, choices=variation_category_choice)
         variation_value = models.CharField(max_length=100)
         is_active = models.BooleanField(default=True)
-        created_date = models.DateTimeField(auto_now=True)
+        created_date = models.DateTimeField(default=True)
         modified_date = models.DateTimeField(auto_now=True)
 
         objects = VariationManager()
