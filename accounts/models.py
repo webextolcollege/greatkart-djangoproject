d194c25d228757f9e80d73e06a38b0ab4adec2d0
Aijaz Ali Inayat <webextolcollege@gmail.com>
2025-01-20 02:38:34 +0500

    not receiving email after registeration

--- accounts/models.py
+++ accounts/models.py
@@ -44,7 +44,7 @@ class Account(AbstractBaseUser):
     last_name = models.CharField(max_length=50)
     username    = models.CharField(max_length=50, unique=True)
     email       = models.EmailField(max_length=100, unique=True)
-    phone_number= models.IntegerField()
+    phone_number= models.CharField(max_length=15, null=True) 
 
     # REQUIRED_FIELDS
 
